a16497d502c50883a832870a16b6c22db851c229
Freeks-Yuta <yutamoriya0818+1@gmail.com>
2022-08-24 17:47:52 +0900

    質問
    ご確認お願いします。

---
+++ JavaStudy/study/src/study/Main.java
@@ -0,0 +1,17 @@
+package study;
+
+
+public class Main{
+	
+	public static void main(String[] args) {
+	}
+	
+	Main() {
+		
+		Super box = new Super();
+		
+		System.out.println(box.a);
+		System.out.println(box.b);
+		System.out.println(box.c);
+	}	
+}
---
+++ JavaStudy/study/src/study/Super.java
@@ -0,0 +1,17 @@
+package study;
+
+
+//Superクラス
+public class Super {
+	
+	String a;
+	String b;
+	String c;
+
+	Super() {
+		
+		String a = "1";
+		String b = "2";
+		String c = "3";
+	}
+}
