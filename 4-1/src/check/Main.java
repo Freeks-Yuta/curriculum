a9b35ef417b2503b8e4a26b062678262cc13716b
Freeks-Yuta <yutamoriya0818+1@gmail.com>
2022-09-03 14:38:20 +0900

    4章チェックテスト
    確認お願いします。

--- 4-1/src/check/Main.java
+++ 4-1/src/check/Main.java
@@ -6,18 +6,19 @@ public class Main {
 	private String firstName = "Moriya";
 	private String lastName = "Yuta";
 	
-	private String printName(String firstName, String lastName) {
+	private static String printName(String firstName, String lastName) {
 		
-		return String printNameBox = "pritnNameメソッド ⇨ " + firstName + lastName;
+		return "pritnNameメソッド ⇨ " + firstName + lastName;
 	}
+	
 	
 	public static void main(String[] args) {
 		
 		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
 		RobotPet robotpet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
-		printName("七海", "真弥");
 		
-		System.out.println(printNameBox);
+		
+		System.out.println(printName("七海", "真弥"));
 		pet.introduce();
 		robotpet.introduce();
 		
