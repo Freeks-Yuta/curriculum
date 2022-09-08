ace139e138adb0cf5e343562eeba2ba689ba2017
Freeks-Yuta <yutamoriya0818+1@gmail.com>
2022-09-08 15:47:48 +0900

    4章チェックテスト修正
    変数の使用

--- 4-1/src/check/Main.java
+++ 4-1/src/check/Main.java
@@ -18,7 +18,11 @@ public class Main {
 		RobotPet robotpet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
 		
 		
-		System.out.println(printName("七海", "真弥"));
+		//System.out.println(printName("七海", "真弥"));
+		//ここに変数を使用しましょう
+		String a = "七海";
+		String b = "真弥";
+		System.out.println(printName(a, b));
 		pet.introduce();
 		robotpet.introduce();
 		
